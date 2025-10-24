069fbaacf96f59ea2af1d374a0aa8f46fcb76d9b
romperwolf <djrojax42@gmail.com>
2025-10-24 21:12:05 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+    function mys(){
+document.getElementById("matchinfo").innerHTML = "KSZ Romper Wolf vs Olimpia Kleczanów";
+document.getElementById("god").innerHTML = "Sobota 15:00";
+document.getElementById("wynik").innerHTML = "0:0";
+//document.getElementById("przegrana").innerHTML = "X 20:00";//
+//document.getElementById("przegrana").style.color = "red";//
+//document.getElementById("wygrana").innerHTML = "X 20:00";
+//document.getElementById("wygrana").style.color = "green";
+alert("Działa");
+    }
+    window.onload=mys;
