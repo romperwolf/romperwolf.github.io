144a2a469bf841c966b1e2d330c7d439fafc18a6
romperwolf <djrojax42@gmail.com>
2025-11-16 13:00:54 +0100

    Add files via upload

---
+++ filesgen.js
@@ -0,0 +1,10 @@
+        function files1() {
+            document.write("<code>")
+            document.write("<p>wspol.html");
+            document.write("<p>archiwe.html");
+            document.write("<p>fots.html");
+            document.write("<p>kontakt.html");
+            document.write("<p>socjal.html");
+            document.write("<P>info.html");
+        }
+         //plik filesgen.js//
