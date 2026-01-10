13a9d27da1443e68093d1345f7e78132b3142ac6
romperwolf <djrojax42@gmail.com>
2026-01-10 22:35:59 +0100

    Add files via upload

---
+++ database.js
@@ -0,0 +1,6 @@
+document.getElementById("matchinfos").innerText = "";
+document.getElementById("matchinfos2").innerText = "";
+document.getElementById("matchinfos3").innerText = "";
+document.getElementById("matchinfos4").innerText = "";
+document.getElementById("errorsinfo").style.display = "block";
+document.getElementById("errorsinfo").innerText = "";
