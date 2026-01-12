3dc6e22f161ddc7edf5d23c302ce8ce505c3cad5
romperwolf <djrojax42@gmail.com>
2026-01-12 23:03:32 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 document.getElementById("matchinfos").innerText = ""; //Przeciwnik//
 document.getElementById("matchinfos2").innerText = "";//Data//
-document.getElementById("matchinfos3").innerText = "19:00";//Godzina//
-document.getElementById("matchinfos4").innerText = "Orlik Jedynka";//Miejsce//
+document.getElementById("matchinfos3").innerText = "";//Godzina//
+document.getElementById("matchinfos4").innerText = "";//Miejsce//
 document.getElementById("errorsinfo").style.display = "block";
 document.getElementById("errorsinfo").innerText = "";
+
 
 
 
