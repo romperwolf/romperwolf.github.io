7870e1ff0d894a1b519272d60a2266ad034668db
romperwolf <djrojax42@gmail.com>
2026-01-12 22:55:26 +0100

    Update database.js

--- database.js
+++ database.js
@@ -4,8 +4,6 @@ document.getElementById("matchinfos3").innerText = "19:00";
 document.getElementById("matchinfos4").innerText = "Orlik Jedynka";
 document.getElementById("errorsinfo").style.display = "block";
 document.getElementById("errorsinfo").innerText = "";
-//errorcode//
-document.getElementById("errorcode").style.display = "block";
-document.getElementById("errorcode").innerText = "";
+
 
 
