c88490e0e997f8021c670118f2df4f0d7e5237d1
romperwolf <djrojax42@gmail.com>
2026-01-12 22:59:37 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
-document.getElementById("matchinfos").innerText = "Zawisza Garbów";
-document.getElementById("matchinfos2").innerText = "12.01.2026";
-document.getElementById("matchinfos3").innerText = "19:00";
-document.getElementById("matchinfos4").innerText = "Orlik Jedynka";
+document.getElementById("matchinfos").innerText = ""; //Przeciwnik//
+document.getElementById("matchinfos2").innerText = "";//Data//
+document.getElementById("matchinfos3").innerText = "19:00";//Godzina//
+document.getElementById("matchinfos4").innerText = "Orlik Jedynka";//Miejsce//
 document.getElementById("errorsinfo").style.display = "block";
 document.getElementById("errorsinfo").innerText = "";
+
 
 
 
