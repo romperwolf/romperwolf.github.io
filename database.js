153165a0e92e82ac01147b0f6bc46196a0f8856d
romperwolf <djrojax42@gmail.com>
2026-02-01 18:56:27 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
-document.getElementById("matchinfos").innerText = ""; //Przeciwnik//
+document.getElementById("matchinfos").innerText = "Brak"; //Przeciwnik//
 document.getElementById("matchinfos2").innerText = "";//Data//
 document.getElementById("matchinfos3").innerText = "";//Godzina//
 document.getElementById("matchinfos4").innerText = "";//Miejsce//
 document.getElementById("errorsinfo").style.display = "block";
 document.getElementById("errorsinfo").innerText = "";
+
 
 
 
